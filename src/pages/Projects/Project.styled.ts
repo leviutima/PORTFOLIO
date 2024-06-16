1bddaf957b1709f695533b267666b15ad0ba96f7
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-06-15 23:03:56 -0300

    aplicando cards na pagina projeto

---
+++ src/pages/Projects/Project.styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ProjetoSection = styled.section `
+    display: flex;
+    flex-direction: column;
+    gap: 100px;
+    margin-left: 10%;
+    padding-bottom: 5%;
+`
+
+export const SectionProjeto = styled.div `
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+`
+
+export const ProjetoCard = styled.div `
+    display: flex;
+    flex-direction: row;
+    gap: 80px;
+`
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,7 +1,40 @@
+import Card from "../../components/Card/Card"
+import { ProjetoCard, ProjetoSection, SectionProjeto } from "./Project.styled"
+import musicList from '../../assets/projetos/projeto 1.png'
+import oceania from '../../assets/projetos/projeto 2.png'
+import SalesF from '../../assets/projetos/projeto 3.png'
+import linkTree from '../../assets/projetos/linktree.png'
+import cineTag from '../../assets/projetos/CINETAG.png'
+import pomodoro from '../../assets/projetos/pomodoro.png'
+import spaceApp from '../../assets/projetos/spaceApp.png'
+
 const Projetos = () => {
     return(
         <>
-
+            <ProjetoSection>
+                <SectionProjeto>
+                    <h2>PROJETOS DE FACULDADE</h2>
+                    <ProjetoCard>
+                        <Card img={oceania} titulo="OCEÂNIA"/>
+                        <Card img={SalesF} titulo="Sales Force"/>
+                    </ProjetoCard>
+                </SectionProjeto>
+                <SectionProjeto>
+                    <h2>PROJETOS AUTORAIS</h2>
+                    <ProjetoCard>
+                        <Card img={musicList} titulo='My Music List'/>
+                        <Card img={linkTree} titulo="LinkTree"/>
+                    </ProjetoCard>
+                </SectionProjeto>
+                <SectionProjeto>
+                    <h2>PROJETOS DE CURSOS</h2>
+                    <ProjetoCard>
+                        <Card img={cineTag} titulo='CineTag'/>
+                        <Card img={pomodoro} titulo="Pomodoro"/>
+                        <Card img={spaceApp} titulo="Space-App"/> 
+                    </ProjetoCard>
+                </SectionProjeto>
+            </ProjetoSection>
         </>
     )
 }
